f633c26900b1e35bc5817ef5e852bd1ea15fec41
Timilehin Sogbesan <sogbesansegun22@gmail.com>
2023-10-29 13:56:44 +0100

    feat: main and mobile pageheader, cart and product search components added

--- src/app/(lobby)/layout.tsx
+++ src/app/(lobby)/layout.tsx
@@ -1,4 +1,3 @@
-// import { SiteFooter } from "@/components/layouts/site-footer"
 import { SiteHeader } from "@/components/layouts/site-header";
 
 export default async function LobbyLayout({
@@ -16,7 +15,6 @@ export default async function LobbyLayout({
     <div className="relative flex min-h-screen flex-col">
       <SiteHeader user={user} />
       <main className="flex-1">{children}</main>
-      {/* <SiteFooter /> */}
     </div>
   );
 }
--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,4 +1,5 @@
 import { Shell } from "@/components/shells/shell";
+import { Balancer } from "react-wrap-balancer";
 
 export default function IndexPage() {
   return (
@@ -8,7 +9,15 @@ export default function IndexPage() {
         aria-labelledby="hero-heading"
         className="mx-auto flex w-full max-w-[64rem] flex-col items-center justify-center gap-4 py-12 text-center md:pt-32"
       >
-        Lobby
+        <Balancer
+          as="h1"
+          className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl"
+        >
+          Nexmart Lobby
+        </Balancer>
+        <Balancer className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+          Built with Nextjs, Typescript, Zod, Radix UI 
+        </Balancer>
       </section>
     </Shell>
   );
