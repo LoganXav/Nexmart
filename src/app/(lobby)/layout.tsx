ed4ad0dd2d68836a9be26f201318afd97085c077
Timilehin Sogbesan <sogbesansegun22@gmail.com>
2023-10-29 14:11:12 +0100

    feat: main and mobile pagefooter added

--- src/app/(lobby)/layout.tsx
+++ src/app/(lobby)/layout.tsx
@@ -1,3 +1,4 @@
+import { SiteFooter } from "@/components/layouts/site-footer"
 import { SiteHeader } from "@/components/layouts/site-header";
 
 export default async function LobbyLayout({
@@ -15,6 +16,7 @@ export default async function LobbyLayout({
     <div className="relative flex min-h-screen flex-col">
       <SiteHeader user={user} />
       <main className="flex-1">{children}</main>
+      <SiteFooter />
     </div>
   );
 }
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,15 @@
 export type SiteConfig = typeof siteConfig;
 
+import type { FooterItem } from "@/types"
+
+
 const links = {
-    twitter: "https://twitter.com",
-    github: "https://github.com",
-    githubAccount: "https://github.com",
-    discord: "https://discord.com/users",
-    calDotCom: "https://cal.com",
-  }
+  twitter: "https://twitter.com",
+  github: "https://github.com",
+  githubAccount: "https://github.com",
+  discord: "https://discord.com/users",
+  calDotCom: "https://cal.com",
+};
 
 export const siteConfig = {
   name: "nexmart",
@@ -15,4 +18,117 @@ export const siteConfig = {
   url: "",
   ogImage: "",
   links,
+
+  footerNav: [
+    {
+      title: "Credits",
+      items: [
+        {
+          title: "OneStopShop",
+          href: "https://onestopshop.jackblatch.com",
+          external: true,
+        },
+        {
+          title: "Acme Corp",
+          href: "https://acme-corp.jumr.dev",
+          external: true,
+        },
+        {
+          title: "craft.mxkaske.dev",
+          href: "https://craft.mxkaske.dev",
+          external: true,
+        },
+        {
+          title: "Taxonomy",
+          href: "https://tx.shadcn.com/",
+          external: true,
+        },
+        {
+          title: "shadcn/ui",
+          href: "https://ui.shadcn.com",
+          external: true,
+        },
+        {
+          title: "Sandmann",
+          href: "https://skateshop.sadmn.com/",
+          external: true,
+        },
+      ],
+    },
+    {
+      title: "Help",
+      items: [
+        {
+          title: "About",
+          href: "/about",
+          external: false,
+        },
+        {
+          title: "Contact",
+          href: "/contact",
+          external: false,
+        },
+        {
+          title: "Terms",
+          href: "/terms",
+          external: false,
+        },
+        {
+          title: "Privacy",
+          href: "/privacy",
+          external: false,
+        },
+      ],
+    },
+    {
+      title: "Social",
+      items: [
+        {
+          title: "Twitter",
+          href: links.twitter,
+          external: true,
+        },
+        {
+          title: "GitHub",
+          href: links.githubAccount,
+          external: true,
+        },
+        {
+          title: "Discord",
+          href: links.discord,
+          external: true,
+        },
+        {
+          title: "cal.com",
+          href: links.calDotCom,
+          external: true,
+        },
+      ],
+    },
+    {
+      title: "Lofi",
+      items: [
+        {
+          title: "beats to study to",
+          href: "https://www.youtube.com/watch?v=jfKfPfyJRdk",
+          external: true,
+        },
+        {
+          title: "beats to chill to",
+          href: "https://www.youtube.com/watch?v=rUxyKA_-grg",
+          external: true,
+        },
+        {
+          title: "a fresh start",
+          href: "https://www.youtube.com/watch?v=rwionZbOryo",
+          external: true,
+        },
+        {
+          title: "coffee to go",
+          href: "https://www.youtube.com/watch?v=2gliGzb2_1I",
+          external: true,
+        },
+      ],
+    },
+  ] satisfies FooterItem[],
 };
